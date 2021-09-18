6cb3bfb5255f812a703f880809d287a5b10040bd
Shayan <sjizan@outlook.com>
2021-09-18 18:13:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from detect_car import capture_plate
+import pandas as pd
+
+# Path to video
+input_path = "Resources/Videos/Traffic1.mp4"
+
+# 2D Matrix of license plates [[license, date]]
+license_plates = capture_plate(input_path)
+
+df = pd.read_csv("lookup_table.csv")
+
+for i in range(len(df)):
+    for j in range(len(license_plates)):
+        if df.iloc[i]['License Plate'] == license_plates[j][0]:
+            df.loc[i, 'Most Recent Date'] = str(license_plates[j][1])
+            print(f"Charging {df.iloc[i]['Owner']}...")
+
+df.to_csv("lookup_table.csv", index=False)
