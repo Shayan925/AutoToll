220a046366077e75e934e48b6878fcccad6e02c9
= <69162332+PilotPrix@users.noreply.github.com>
2021-09-18 19:53:54 -0400

    Update: one image OCR file

--- ocr.py
+++ ocr.py
@@ -1,16 +1,26 @@
-import os, io
-from google.cloud import vision
-from google.cloud.vision import types
+import io
+import os
+from google.cloud import vision # Imports the Google Cloud client library
 
+# Authentication
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'VisionAPI_Service_Account.json'
 
+# Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-FILE_NAME = 'Plate1.jpg'
-FOLDER_PATH = ''
-with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
-    content = image_file.read()
 
-image = vision.types.Image(content=content)
+def detectText(img):
+    # Loads the image into memory
+    with io.open(img, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
 
-response = client.text_detection(image=image)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    for text in texts:
+        print(text.local)
+    
+
+
+file_name = os.path.abspath('Plate1.jpg')
+detectText(file_name)
